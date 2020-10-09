cd90850655c575a93db9303e7ace41bd37cdf2fe
lrh21g <837233792@qq.com>
2020-10-10 00:52:39 +0800

    ValidateForm、ValidateInput组件

---
+++ src/components/ValidateInput.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="validate-input-container pb-3">
+    <input
+      v-if="tag !== 'textarea'"
+      v-model="inputRef.val"
+      v-bind="$attrs"
+      class="form-control"
+      :class="{ 'is-invalid': inputRef.error }"
+      @blur="validateInput"
+    />
+    <textarea
+      v-else
+      v-model="inputRef.val"
+      v-bind="$attrs"
+      class="form-control"
+      :class="{ 'is-invalid': inputRef.error }"
+      @blur="validateInput"
+    >
+    </textarea>
+    <span v-if="inputRef.error" class="invalid-feedback">
+      {{ inputRef.message }}
+    </span>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, PropType, onMounted, computed } from "vue";
+import { emitter } from "./ValidateForm";
+
+const emailReg = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+interface RuleProp {
+  type: "required" | "email" | "custom";
+  message: string;
+  validator?: () => boolean;
+}
+
+export type RulesProp = RuleProp[];
+export type TagType = "input" | "textarea";
+export default defineComponent({
+  name: "ValidateInput",
+  props: {
+    rules: Array as PropType<RulesProp>,
+    modelValue: String,
+    tag: {
+      type: String as PropType<TagType>,
+      default: "input"
+    }
+  },
+  inheritAttrs: false,
+  setup(props, context) {
+    const inputRef = reactive({
+      val: computed({
+        get: () => props.modelValue | "",
+        set: val => {
+          context.emit("update:modelValue", val);
+        }
+      }),
+      error: false,
+      message: ""
+    });
+    const validateInput = () => {
+      if (props.rules) {
+        const allPassed = props.rules.every(rule => {
+          let passed = true;
+          inputRef.message = rule.message;
+          switch (rule.type) {
+            case "required":
+              passed = inputRef.val.trim() !== "";
+              break;
+            case "email":
+              passed = emailReg.test(inputRef.val);
+              break;
+            case "custom":
+              passed = rule.validator ? rule.validator() : true;
+              break;
+            default:
+              break;
+          }
+        });
+        inputRef.error = !allPassed;
+        return allPassed;
+      }
+    };
+    onMounted(() => {
+      emitter.emit("form-item-created", validateInput);
+    });
+    return {
+      inputRef,
+      validateInput
+    };
+  }
+});
+</script>
